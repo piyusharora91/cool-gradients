d3708fbcadbaae0dc7e0ddc14da12f4156634594
Piyush <piyusharora596@gmail.com>
2023-03-05 13:00:37 +0530

    color updates

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-var appContainer = document.getElementById('gradient-container');
-var gradientValueDisplay = document.getElementById('value-display');
-var color1 = document.getElementById('color1');
-var color2 = document.getElementById('color2');
-var resetValuesButton = document.getElementById('reset-color-values');
+var appContainer = document.getElementById("gradient-container");
+var gradientValueDisplay = document.getElementById("value-display");
+var color1 = document.getElementById("color1");
+var color2 = document.getElementById("color2");
+var resetValuesButton = document.getElementById("reset-color-values");
 var loadSavedValues = function () {
-    // let savedColor1Value = localStorage.getItem('color1') as string;
-    // let savedColor2Value = localStorage.getItem('color2') as string;
-    var savedColor1Value = localStorage.getItem('color1');
-    var savedColor2Value = localStorage.getItem('color2');
-    if (typeof savedColor1Value === 'string' && typeof savedColor2Value === 'string') {
+    var savedColor1Value = localStorage.getItem("color1");
+    var savedColor2Value = localStorage.getItem("color2");
+    if (typeof savedColor1Value === "string" &&
+        typeof savedColor2Value === "string") {
         color1.value = savedColor1Value;
         color2.value = savedColor2Value;
         changeGradientAndHeading();
@@ -19,6 +18,7 @@ var changeGradientAndHeading = function () {
     gradientValueDisplay.textContent = appContainer.style.background;
 };
 var changeColorInput = function () {
+    // when user provides input this method runs
     changeGradientAndHeading();
     // Store the color values in the local storage
     localStorage.setItem("color1", color1.value);
@@ -27,14 +27,14 @@ var changeColorInput = function () {
 var eraseColorValues = function () {
     localStorage.removeItem("color1");
     localStorage.removeItem("color2");
-    color1.value = '#4abde3';
-    color2.value = '#2ed64a';
+    color1.value = "#4abde3";
+    color2.value = "#D68765";
     changeGradientAndHeading();
 };
 //set the color if values are present in local storage
-if (localStorage.getItem('color1') && localStorage.getItem('color2')) {
+if (localStorage.getItem("color1") && localStorage.getItem("color2")) {
     loadSavedValues();
 }
-color1.addEventListener('input', changeColorInput);
-color2.addEventListener('input', changeColorInput);
-resetValuesButton.addEventListener('click', eraseColorValues); // removes color values from local storage
+color1.addEventListener("input", changeColorInput);
+color2.addEventListener("input", changeColorInput);
+resetValuesButton.addEventListener("click", eraseColorValues); // removes color values from local storage
--- script.ts
+++ script.ts
@@ -1,48 +1,56 @@
-const appContainer = document.getElementById('gradient-container') as HTMLDivElement;
-const gradientValueDisplay = document.getElementById('value-display') as HTMLHeadingElement;
-const color1 = document.getElementById('color1') as HTMLInputElement;
-const color2 = document.getElementById('color2') as HTMLInputElement;
-const resetValuesButton = document.getElementById('reset-color-values') as HTMLButtonElement;
+const appContainer = document.getElementById(
+  "gradient-container"
+) as HTMLDivElement;
+const gradientValueDisplay = document.getElementById(
+  "value-display"
+) as HTMLHeadingElement;
+const color1 = document.getElementById("color1") as HTMLInputElement;
+const color2 = document.getElementById("color2") as HTMLInputElement;
+const resetValuesButton = document.getElementById(
+  "reset-color-values"
+) as HTMLButtonElement;
 
 const loadSavedValues = () => {
-    // let savedColor1Value = localStorage.getItem('color1') as string;
-    // let savedColor2Value = localStorage.getItem('color2') as string;
-    let savedColor1Value = localStorage.getItem('color1');
-    let savedColor2Value = localStorage.getItem('color2');
-    if (typeof savedColor1Value === 'string' && typeof savedColor2Value === 'string') {
-        color1.value = savedColor1Value;
-        color2.value = savedColor2Value;
-        changeGradientAndHeading();
-    }
-}
+  let savedColor1Value = localStorage.getItem("color1");
+  let savedColor2Value = localStorage.getItem("color2");
+  if (
+    typeof savedColor1Value === "string" &&
+    typeof savedColor2Value === "string"
+  ) {
+    color1.value = savedColor1Value;
+    color2.value = savedColor2Value;
+    changeGradientAndHeading();
+  }
+};
 
 const changeGradientAndHeading = () => {
-    appContainer.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
-    gradientValueDisplay.textContent = appContainer.style.background;
-}
+  appContainer.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  gradientValueDisplay.textContent = appContainer.style.background;
+};
 
-const changeColorInput = () => {    // when user provides input this method runs
-    changeGradientAndHeading();
+const changeColorInput = () => {
+  // when user provides input this method runs
+  changeGradientAndHeading();
 
-    // Store the color values in the local storage
-    localStorage.setItem("color1", color1.value);
-    localStorage.setItem("color2", color2.value);
-}
+  // Store the color values in the local storage
+  localStorage.setItem("color1", color1.value);
+  localStorage.setItem("color2", color2.value);
+};
 
 const eraseColorValues = () => {
-    localStorage.removeItem("color1");
-    localStorage.removeItem("color2");
-    color1.value = '#4abde3';
-    color2.value = '#2ed64a';
-    changeGradientAndHeading();
-}
+  localStorage.removeItem("color1");
+  localStorage.removeItem("color2");
+  color1.value = "#4abde3";
+  color2.value = "#D68765";
+  changeGradientAndHeading();
+};
 
 //set the color if values are present in local storage
-if (localStorage.getItem('color1') && localStorage.getItem('color2')) {
-    loadSavedValues();
+if (localStorage.getItem("color1") && localStorage.getItem("color2")) {
+  loadSavedValues();
 }
 
-color1.addEventListener('input', changeColorInput);
-color2.addEventListener('input', changeColorInput);
+color1.addEventListener("input", changeColorInput);
+color2.addEventListener("input", changeColorInput);
 
-resetValuesButton.addEventListener('click', eraseColorValues);  // removes color values from local storage
+resetValuesButton.addEventListener("click", eraseColorValues); // removes color values from local storage
